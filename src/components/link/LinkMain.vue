7c5a674bc92e2c52c66599133d70e1f825565141
mininjin <mininjin332@gmail.com>
2022-06-06 14:03:22 +0900

    remove animation at Link

--- src/components/link/LinkMain.vue
+++ src/components/link/LinkMain.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="py-10 flex flex-wrap">
-    <a v-for="(item, i) in list" :key="item.key" :href="item.href" target="_blank" rel="noopener noreferrer" :class="`w-full md:w-1/2 p-5 transition-all duration-1000 transform ${open[i] ? '' : 'translate-y-10 opacity-0'
-    }`" :ref="setRefs">
+    <a v-for="(item, i) in list" :key="item.key" :href="item.href" target="_blank" rel="noopener noreferrer"
+      class="w-full md:w-1/2 p-5 transition-all">
       <img :src="item.src" :alt="item.name" class="object-contain w-full" />
       <div class="w-full p-3 font-strong text-xl underline text-center">
         {{ item.name }}
@@ -12,8 +12,7 @@
 
 <script lang="ts" setup>
 import { LINK_IMAGE_PATH } from "@/constants";
-import { checkScrollPosition } from "@/plugins/checkScrollPosition";
-import { computed, onMounted, ref } from "vue";
+import { computed, ref } from "vue";
 
 type LinkElement = { src: string; href: string; name: string };
 const LINK_LIST: LinkElement[] = [
@@ -32,23 +31,5 @@ const LINK_LIST: LinkElement[] = [
 const list = computed(() =>
   LINK_LIST.map((v, i) => ({ ...v, key: v.name + i }))
 );
-const open = ref<boolean[]>([]);
-const refs = ref<HTMLElement[]>([]);
-const setRefs = (ref: any) => {
-  if (ref instanceof HTMLElement) {
-    refs.value.push(ref);
-    open.value.push(false);
-  }
-};
-const checkScroll = () => {
-  refs.value.forEach((v, i) => {
-    open.value.splice(i, 1, checkScrollPosition(v));
-  });
-};
-
-onMounted(() => {
-  checkScroll();
-  window.addEventListener("scroll", checkScroll);
-});
 
 </script>
