fa8cc427154ecaf22951dd3a30f990b1bc1d40d5
mininjin <mininjin332@gmail.com>
2022-07-13 12:14:35 +0900

    add autoplay

--- src/components/home/HomeAtmosphere.vue
+++ src/components/home/HomeAtmosphere.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="py-10 overflow-hidden">
     <SectionTitle title="お店の雰囲気" />
-    <div class="my-16 relative p-2">
-      <iframe title="3d-image" width='853' height='480' src='https://my.matterport.com/show/?m=TkZg8FDDMJX'
-        frameborder='0' allowfullscreen allow='xr-spatial-tracking' class="mx-auto w-full"></iframe>
+    <div class="my-16 relative p-2 aspect-square flex items-center justify-center">
+      <iframe title="3d-image" width='853' height='480' src='https://my.matterport.com/show/?m=TkZg8FDDMJX&play=1'
+        frameborder="0" allowfullscreen allow="xr-spatial-tracking" class="mx-auto h-full"></iframe>
     </div>
   </section>
 </template>
